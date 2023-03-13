c82f97135457e40eef5e3085b2d042ed33a7b903
Stage <99012641+PacoV12@users.noreply.github.com>
2023-03-13 17:41:39 +0100

    Add files via upload

---
+++ formulaire (2).php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1>Formulaire de saisie</h1>
+<form action="lacarte.php" method="POST">
+<p>saisir la latitude <input name="latitude"/></p>
+<p>saisir la longitude <input name="longitude"/></p>
+<p><input type="submit"/></p>
+</form>
+</body>
+</html>
